7c7d5f8074e1a0b804f2f7e8cf86284c61205533
salvabolso17-wq <salvabolso17@gmail.com>
2026-05-06 20:39:10 +0000

    corrigindo pool postgres

--- src/db/client.ts
+++ src/db/client.ts
@@ -3,10 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = new Pool({
+const pool = new Pool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
 });
+
+export default pool;
