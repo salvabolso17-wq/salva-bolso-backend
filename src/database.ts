7ad3cf5824429052e03c1a40c75f0cd707e545b4
salvabolso17-wq <salvabolso17@gmail.com>
2026-05-06 22:18:48 +0000

    criando tabela transactions

--- src/database.ts
+++ src/database.ts
@@ -15,7 +15,19 @@ export async function createTables() {
       );
     `);
 
-    console.log("Tabela users criada/verificada ✅");
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS transactions (
+        id SERIAL PRIMARY KEY,
+        user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+        tipo VARCHAR(20) NOT NULL,
+        valor NUMERIC(10,2) NOT NULL,
+        categoria VARCHAR(100),
+        descricao TEXT,
+        criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+
+    console.log("Tabelas criadas/verificadas ✅");
   } catch (error) {
     console.error("ERRO REAL DO BANCO:");
     console.error(error);
