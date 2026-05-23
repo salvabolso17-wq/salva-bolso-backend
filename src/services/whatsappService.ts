a421f537111128e9336b4734055f28dece1bf463
Salva Bolso <devappmina.a@gmail.com>
2026-05-23 15:29:25 -0500

    fix: textoTrim fora de escopo na deteccao do zerar

--- src/services/whatsappService.ts
+++ src/services/whatsappService.ts
@@ -1090,7 +1090,7 @@ export async function processWhatsAppMessage(message: NormalizedMessage): Promis
     return await handleDetalhadoCommand(user, message.telefone);
   }
 
-  if (/^(zerar?|resetar?|apagar?\s+tudo|come[çc]ar?\s+do\s+zero|limpar?\s+gastos?|zerar?\s+gastos?|zerar?\s+tudo|apagar?\s+gastos?\s+do\s+m[eê]s)[\?!.]*$/i.test(textoTrim)) {
+  if (/^(zerar?|resetar?|apagar?\s+tudo|come[çc]ar?\s+do\s+zero|limpar?\s+gastos?|zerar?\s+gastos?|zerar?\s+tudo|apagar?\s+gastos?\s+do\s+m[eê]s)[\?!.]*$/i.test(message.texto.trim())) {
     try {
       return await handleZerarIniciar(user, message.telefone);
     } catch (err) {
